37cfd89b2af538840cc7f2250a4e65465282d105
Ivan Kozma <Kozmaivan2013@gmail.com>
2025-04-16 23:26:14 +0400

    reorganize project structure

--- products/serializers.py
+++ products/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from ..models import Product, ProductCategory
+from .models import Product, ProductCategory
 
 class ProductCategorySerializer(serializers.ModelSerializer):
     class Meta:
--- settings.py
+++ settings.py
@@ -16,7 +16,7 @@ from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-sys.path.insert(0, str(BASE_DIR / 'apps'))
+sys.path.insert(0, str(BASE_DIR))
 
 
 # Quick-start development settings - unsuitable for production
